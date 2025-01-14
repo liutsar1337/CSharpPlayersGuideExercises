97c1d27438c7653d375877dcd66fce21cbad0ee7
liutsar <liutsar346@gmail.com>
2025-01-14 14:09:45 +0200

    Solve "TheTriangleFarmer" exercise

---
+++ Part01/Level07-Math/TheTriangleFarmer/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input triangle's base: ");
+string triangleBaseText = Console.ReadLine();
+double triangleBase = Convert.ToDouble(triangleBaseText);
+
+Console.WriteLine("Input triangle's height: ");
+string triangleHeightText = Console.ReadLine();
+double triangleHeight = Convert.ToDouble(triangleHeightText);
+
+double triangleArea = (triangleBase * triangleHeight) / 2;
+
+Console.WriteLine("Triangle Area: " + triangleArea);
