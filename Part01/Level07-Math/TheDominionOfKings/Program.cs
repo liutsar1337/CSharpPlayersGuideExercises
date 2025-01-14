eb4adbd80a09ebb9a8f2d2a00bf7aa0535af2e72
liutsar <liutsar346@gmail.com>
2025-01-14 17:42:18 +0200

    Solve "The Dominion of Kings" exercise

---
+++ Part01/Level07-Math/TheDominionOfKings/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter the number of provinces you have: ");
+int provinceCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of duchies you have: ");
+int duchyCount = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of estates you have: ");
+int estateCount = Convert.ToInt32(Console.ReadLine());
+
+int totalPoints = provinceCount * 6 + duchyCount * 3 + estateCount;
+Console.WriteLine("You got a total score of " + totalPoints + " points");
