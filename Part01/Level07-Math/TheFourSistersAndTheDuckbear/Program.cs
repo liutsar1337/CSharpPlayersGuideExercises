1d04f4e4713c7e3404ee1cbb405c6352aadef7c7
liutsar <liutsar346@gmail.com>
2025-01-14 17:18:14 +0200

    Solve "The Four Sisters and the Duckbear" exercise

---
+++ Part01/Level07-Math/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Chocolate eggs gathered today: ");
+int chocolateEggsCount = Convert.ToInt32(Console.ReadLine());
+
+int forEachSister = chocolateEggsCount / 4;
+int leftoverForDuckbear = chocolateEggsCount % 4;
+
+Console.WriteLine("Each sister gets " + forEachSister);
+Console.WriteLine("The duckbear gets " + leftoverForDuckbear);
