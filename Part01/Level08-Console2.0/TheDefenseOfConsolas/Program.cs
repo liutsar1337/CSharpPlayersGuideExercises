8738ec6f9e93cf182eb986a095d7aecb0d347c1a
liutsar <liutsar346@gmail.com>
2025-01-15 13:09:48 +0200

    Solve "TheDefenseOfConsolas" exercise

---
+++ Part01/Level08-Console2.0/TheDefenseOfConsolas/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Title = "Defense of Consolas";
+
+Console.Write("Target Row? ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+void DisplayColoredCoordinates(ConsoleColor color, int row, int column)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine($"{row}, {column}");
+}
+
+Console.WriteLine("Deploy squad members to: ");
+DisplayColoredCoordinates(ConsoleColor.Red, targetRow, targetColumn - 1);
+DisplayColoredCoordinates(ConsoleColor.Green, targetRow - 1, targetColumn);
+DisplayColoredCoordinates(ConsoleColor.Blue, targetRow, targetColumn + 1);
+DisplayColoredCoordinates(ConsoleColor.Yellow, targetRow + 1, targetColumn);
+
+Console.Beep();
