7b32ad57781fe455abd8a8864d44edaa7a4d9c92
liutsar <liutsar346@gmail.com>
2025-01-17 18:05:32 +0200

    Solve "Watchtower" exercise

---
+++ Part01/Level09-DecisionMaking/Watchtower/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Enter x value: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter y value: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Coordinates are: x={x}, y={y}");
+
+if (x == 0)
+{
+    if (y > 0)
+        Console.WriteLine("The enemy is to the north!");
+    else if (y == 0)
+        Console.WriteLine("The enemy is here!");
+    else if (y < 0)
+        Console.WriteLine("The enemy is to the south!");
+}
+else if (x < 0)
+{
+    if (y > 0)
+        Console.WriteLine("The enemy is to the northwest!");
+    if (y == 0)
+        Console.WriteLine("The enemy is to the west!");
+    if (y < 0)
+        Console.WriteLine("The enemy is to the southwest!");
+}
+else if (x > 0)
+{
+    if (y > 0)
+        Console.WriteLine("The enemy is to the northeast!");
+    if (y == 0)
+        Console.WriteLine("The enemy is to the east!");
+    if (y < 0)
+        Console.WriteLine("The enemy is to the southeast!");
+}
