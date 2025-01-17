f91714c669bab75f0f83aef78796e0ed31e21e38
liutsar <liutsar346@gmail.com>
2025-01-17 16:55:23 +0200

    Solve "Repairing the Clocktower" exercise

---
+++ Part01/Level09-DecisionMaking/RepairingTheClocktower/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter the number that flows to the clock: ");
+int clockNumber = Convert.ToInt32(Console.ReadLine());
+
+if (clockNumber % 2 == 0) Console.WriteLine("Tick");
+else Console.WriteLine("Tock");
