fc08044a971155b0092da79ec916e18bb4681613
liutsar <liutsar346@gmail.com>
2025-03-04 13:28:17 +0200

    Solve "TheReplicatorOfDTo" exercise

---
+++ Part01/Level12-Arrays/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] original = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    Console.Write($"Enter a number for index {i}: ");
+    int userIndex = Convert.ToInt32(Console.ReadLine());
+    original[i] = userIndex;
+}
+
+int[] replicated = new int[5];
+for (int i = 0; i < 5; i++)
+{
+    replicated[i] = original[i];
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Value at index {i} in a1:{original[i]} and a2:{replicated[i]}");
+}
