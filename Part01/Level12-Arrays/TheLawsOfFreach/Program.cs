37e0b06a0e3ea4d8e466dd74a86223c987bc45c7
liutsar <liutsar346@gmail.com>
2025-03-04 15:36:55 +0200

    Solve "TheLawsOfFreach" exercise

---
+++ Part01/Level12-Arrays/TheLawsOfFreach/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[] { 51, 24, 100, 12, 355 };
+
+int minimum = int.MaxValue;
+foreach (int value in array)
+{
+    if (value < minimum)
+        minimum = value;
+}
+
+int total = 0;
+foreach (int value in array)
+{
+    total += value;
+}
+
+float average = (float)total / array.Length;
+
+Console.WriteLine($"Array's minimum is {minimum} and average is {average}");
