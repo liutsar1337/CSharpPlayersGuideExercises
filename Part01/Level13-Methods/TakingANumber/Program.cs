730c83cf2bb066d404a64abfa4d41cf46e9f5053
liutsar <liutsar346@gmail.com>
2025-03-09 10:46:32 +0200

    Solve "TakingANumber" exercise

---
+++ Part01/Level13-Methods/TakingANumber/Program.cs
@@ -0,0 +1,27 @@
+﻿int result = AskForNumber("What year was the movie Jurassic Park released?");
+Console.WriteLine($"Year: {result}");
+
+int numberInRange = AskFroNumberInRange("Enter the number between 10 and 50:", 10, 50);
+
+Console.WriteLine(numberInRange);
+
+int AskForNumber(string text)
+{
+    Console.Write($"{text} ");
+    int returnNumber = Convert.ToInt32(Console.ReadLine());
+    return returnNumber;
+}
+
+int AskFroNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int number = AskForNumber(text);
+        if (number < min)
+            Console.WriteLine($"{number} is too low");
+        else if (number > max)
+            Console.WriteLine($"{number} is too high");
+        else if (number >= min && number <= max)
+            return number;
+    }
+}
